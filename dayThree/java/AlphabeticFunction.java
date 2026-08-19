aded888633fa7984dda4278afc70616f38f28033
Perezimo <reachingperez@gmail.com>
2026-08-19 17:06:04 +0100

    afternoon

---
+++ dayThree/java/AlphabeticFunction.java
@@ -0,0 +1,38 @@
+
+
+public class AlphabeticFunction{
+
+public static void main(String [] args){
+
+	
+
+	}
+	
+	public static char [] alphaNumeric (char [] chars){
+	
+		chars = createArray();
+
+		int [] counts = countLetters(chars);
+	
+		for(int counts =0; counts <  chars.length; i++){
+		
+			if (chars[count] == "a"){
+			
+				counts[0]++;
+			}
+			else if (chars[count] == "1"){
+			
+			
+				counts[1]++;
+				}
+			else if (chars[count] == "c"){
+			
+			
+				counts[2]++;
+				}
+			else{
+			
+				(chars[count]==0);
+			}
+			
+			
---
+++ dayThree/javaScript/Alphanumeric.js
@@ -0,0 +1,27 @@
+
+
+let chars = createArray();
+
+	let	int [] counts = countLetters(chars);
+	
+		for(int counts =0; counts <  chars.length; i++){
+		
+			if (chars[count] == "a"){
+			
+				counts[0]++;
+			}
+			else if (chars[count] == "1"){
+			
+			
+				counts[1]++;
+				}
+			else if (chars[count] == "c"){
+			
+			
+				counts[2]++;
+				}
+			else{
+			
+				(chars[count]==0);
+			}
+		
